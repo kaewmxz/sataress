2c5297243672fd352bc93337449078c666530496
oakntk <oakoakku@hotmail.com>
2021-12-29 15:52:22 +0700

    test

--- sataress-project/src/App.js
+++ sataress-project/src/App.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
 import Login from './components/login';
-=======
-import { getAuth } from 'firebase/auth'
-import React, { useState, useEffect }  from 'react';
-import Login from './components/Login';
-import Home from './components/Home';
-
->>>>>>> 3b07fc5932c39c81521f3d0dae643ada14f47c1d
 function App() {
 
   const [user, setUser] = useState(null);
@@ -20,13 +12,8 @@ function App() {
   }, []);
   console.log(user);
   return (
-<<<<<<< HEAD
     <div className="App">
       <Login />
-=======
-    <div className="app">
-      {user ? <Home user={user} /> : <Login />}
->>>>>>> 3b07fc5932c39c81521f3d0dae643ada14f47c1d
     </div>
   );
 }
