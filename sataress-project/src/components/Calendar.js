82502ba26979ea587a5b52ac519da62d94af1523
kaewmxz <kaewmxz@gmail.com>
2022-01-03 15:01:20 +0700

    css-subfolder+homepage-update

---
+++ sataress-project/src/components/Calendar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+        <center>
+            Hi, this is calendar page.
+        </center>
+    </div>
+  );
+};
+export default Calendar;
--- sataress-project/src/components/Home.js
+++ sataress-project/src/components/Home.js
@@ -1,16 +1,46 @@
-import React from 'react';
-
-import { auth } from '../services/firebase'
+import React, { useState } from "react";
+import { auth } from "../services/firebase";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 const Home = ({ user }) => {
   var name = user.displayName;
   var uid = user.uid;
+  // These two const used for the weekly/monthly togglebuttons
+  const [alignment, setAlignment] = React.useState("web");
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
   return (
     <div className="home">
-      <h1>Hello, <span></span>{name}</h1>
+      <h1>
+        Hello, <span></span>
+        {name}
+      </h1>
       <button onClick={() => auth.signOut()}>Sign out</button>
+      <center>
+        {/* Calendar card */}
+        <img src="/image/calendar.png" />
+        <br />
+        <br />
+        {/* Gratitude journal button */}
+        <button /*onClick={}*/>Gratitude journal</button>
+        <br />
+        <br />
+        {/* Monthly or Weekly button */}
+        <ToggleButtonGroup
+          color="primary"
+          value={alignment}
+          exclusive
+          onChange={handleChange}
+        >
+          <ToggleButton value="weekly">Weekly</ToggleButton>
+          <ToggleButton value="monthly">Monthly</ToggleButton>
+        </ToggleButtonGroup>
+      </center>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
