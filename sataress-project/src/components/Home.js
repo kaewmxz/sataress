8fde4e8b2bbb68988c39af834f11c5dd2e781d8e
oakntk <oakoakku@hotmail.com>
2022-01-19 15:39:35 +0700

    oak update

--- sataress-project/src/components/Home.js
+++ sataress-project/src/components/Home.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
-import { auth } from "../services/firebase";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Avatar from "@mui/material/Avatar";
 import { withTheme } from "@material-ui/core/styles";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { createTheme } from '@mui/material/styles';
 
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import Paper from "@mui/material/Paper";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Paper from '@mui/material/Paper';
+
+
 
 const Bg = withTheme(styled.div`
   position: fixed;
@@ -47,9 +49,53 @@ const Name = withTheme(styled.div`
   font-family: Roboto;
   font-style: normal;
   font-weight: bold;
-  font-size: 22px;
+  font-size: 16px;
   line-height: 26px;
   text-align: center;
+`);
+
+const Fire = withTheme(styled.div`
+  position: absolute;
+width: 23px;
+height: 30px;
+left: 211px;
+top: 93px;
+`);
+
+const Streak = withTheme(styled.div`
+position: absolute;
+width: 70px;
+height: 12px;
+left: 244px;
+top: 105px;
+font-family: Roboto;
+font-style: normal;
+font-weight: bold;
+font-size: 10px;
+line-height: 12px;
+text-align: center;
+`);
+
+const Calendar = withTheme(styled.div`
+position: absolute;
+left: 36px;
+top: -70px;
+`);
+
+const Gratitude = withTheme(styled.div`
+position: absolute;
+left: 71px;
+top: 123px;
+`);
+
+const Toggle = withTheme(styled.div`
+position: absolute;
+width: 84px;
+height: 25px;
+left: 81px;
+top: 411px;
+
+
 `);
 
 const Home = ({ user }) => {
@@ -69,41 +115,46 @@ const Home = ({ user }) => {
           <Avatar
             alt="Oak Natthakrit"
             src="/broken-image.jpg"
-            sx={{ width: 67, height: 67 }}
-          >
-            O
-          </Avatar>
+            sx={{ width: 67, height: 67 }}>O</Avatar>
         </Link>
       </Profile>
       <Name>Hi,{name}</Name>
-      <img src="/image/fire.png" width="23px"></img>
+      <Fire><img src="/image/fire.png" width="23px"></img></Fire>
+      <Streak>Current Streak</Streak>
       <Bg>
-        <center>
           {/* Calendar card */}
-          <Link to="/Calendar">
-            <img src="/image/calendar.png" />
-          </Link>
-          <br />
-          <br />
+          <Calendar>
+            <Link to="/Calendar">
+              <img src="/image/calendar.png"
+                width="307px"
+                height="182px" />
+            </Link>
+          </Calendar>
           {/* Gratitude journal button */}
-          <button /*onClick={}*/>Gratitude journal</button>
-          <br />
-          <br />
+          <Gratitude>
+            <Link to="/Gratitude">
+              <img src="/image/gratitude.png"
+                width="243px"
+                height="31px" />
+            </Link>
+          </Gratitude>
           {/* Monthly or Weekly button */}
+          <Toggle>
           <ToggleButtonGroup
             color="primary"
             value={alignment}
             exclusive
-            onChange={handleChange}
-          >
-            <ToggleButton value="weekly">Weekly</ToggleButton>
-            <ToggleButton value="monthly">Monthly</ToggleButton>
+            onChange={handleChange}>
+            <ToggleButton value="weekly">สัปดาห์</ToggleButton>
+            <ToggleButton value="monthly">เดือน</ToggleButton>
           </ToggleButtonGroup>
-        </center>
-        <Box>
+          </Toggle>
+          
+
+          {/* <Box sx={{ pb: 7 }}>
           <CssBaseline />
-          <Paper>
-            <BottomNavigation showLabels>
+          <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+            <BottomNavigation showLabels >
               <BottomNavigationAction label="Recents" icon={<FavoriteIcon />} />
               <BottomNavigationAction
                 label="Favorites"
@@ -119,10 +170,11 @@ const Home = ({ user }) => {
               <BottomNavigationAction label="Archive" icon={<FavoriteIcon />} />
             </BottomNavigation>
           </Paper>
-        </Box>
+        </Box> */}
+
+
       </Bg>
     </Head>
   );
 };
-
 export default Home;
