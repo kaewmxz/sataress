dfde48c2b3c9131cfd079b2ad6f5c4bbaf38f144
oakntk <oakoakku@hotmail.com>
2022-02-08 15:42:27 +0700

    oak updatee kode yer ai sus

--- sataress-project/src/components/BottomNavigationBar .js
+++ sataress-project/src/components/BottomNavigationBar .js
@@ -7,6 +7,8 @@ import HealthAndSafetyIcon from '@mui/icons-material/HealthAndSafety';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import Fab from '@mui/material/Fab';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 
 import { Link } from "react-router-dom";
 
@@ -18,7 +20,23 @@ height: 79.44px;
 background: #FFE9E9;
 box-shadow: inset 0px 4px 4px rgba(251, 24, 24, 0.36);
 border-radius: 23px 23px 0px 0px;
-flex-grow:1;
+padding-bottom: 0px;
+margin-top:5px;
+${(props) => props.theme.breakpoints.up("xs")} {
+  margin-left: 0px;
+}
+${(props) => props.theme.breakpoints.up("sm")} {
+  margin-left: 96px;
+}
+${(props) => props.theme.breakpoints.up("md")} {
+  margin-left: 276px;
+}
+${(props) => props.theme.breakpoints.up("lg")} {
+  margin-left: 436px;
+}
+${(props) => props.theme.breakpoints.up("xl")} {
+  margin-left: 756px;
+}
 `);
 
 
@@ -27,6 +45,9 @@ const theme = createTheme({
     Black: {
       main: '#212121',
   },
+  Pink: {
+    main: '#f8bbd0',
+},
 },
 });
 
@@ -45,7 +66,7 @@ export default function BottomNavigationBar() {
             sx={{ fontSize: 40, mt: 3, ml: 4 }} />
         </Link>
         <Link to="/Moodtrack">
-        <Fab  size = "large"
+        <Fab size = "large" 
             sx={{ mt: -5, ml: 4 }} >
             <img src="./image/wlogo.png" width="85"></img>
           </Fab>
