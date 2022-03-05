eb7f0d2c331bf0541328d2e6b5dbb43eede9ea53
oakntk <oakoakku@hotmail.com>
2022-03-05 14:05:20 +0700

    oak update

--- sataress-project/src/components/calendar/Calendar.js
+++ sataress-project/src/components/calendar/Calendar.js
@@ -15,6 +15,7 @@ import CalendarPicker from "@mui/lab/CalendarPicker";
 import { makeStyles } from "@mui/styles";
 import moment from "moment";
 import axios from "axios";
+import { ThemeProvider } from "styled-components";
 
 const minDate = new Date("2022-01-01T00:00:00.000");
 const maxDate = new Date("2022-12-31T00:00:00.000");
@@ -31,7 +32,7 @@ const Bg = withTheme(styled.div`
     #ffbdbd 95.83%
   );
 `);
-const styles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   dayWithDotContainer: {
     position: "relative",
   },
@@ -41,14 +42,14 @@ const styles = makeStyles((theme) => ({
     width: 0,
     border: "2px solid",
     borderRadius: 4,
-    borderColor: theme.palette.primary.main,
     right: "50%",
     transform: "translateX(1px)",
     top: "80%",
   },
-}));
+});
 
-const Calendarcard = () => {
+export default function Calendarcard() {
+  const classes = useStyles();
   const { currentUser } = useContext(AuthContext);
   // Another lib
   // const [selectedDay, setSelectedDay] = useState(null);
@@ -56,7 +57,7 @@ const Calendarcard = () => {
   const [daysWithDot, setDaysWithDot] = useState([]);
   let navigate = useNavigate();
   console.log(date)
-  // const classes = styles();
+
   if (!currentUser) {
     return (
       <Routes>
@@ -66,12 +67,12 @@ const Calendarcard = () => {
   }
 
   const onPickerViewChange = async (date) => {
-    const variables = [moment(date).clone().startOf("month").format("M/D/YYYY"),moment(date).clone().endOf("month").format("MM/D/YYYY")];
-      // fromDate: moment(date).clone().startOf("month").format("M/D/YYYY"),
-      // toDate: moment(date).clone().endOf("month").format("MM/D/YYYY"),
-    
+    const variables = [moment(date).clone().startOf("month").format("M/D/YYYY"), moment(date).clone().endOf("month").format("MM/D/YYYY")];
+    // fromDate: moment(date).clone().startOf("month").format("M/D/YYYY"),
+    // toDate: moment(date).clone().endOf("month").format("MM/D/YYYY"),
+
     console.log(variables);
-    
+
     try {
       const result = await axios.get("http://localhost:4000/mood-dates", {
         params: { id: currentUser.uid, date: variables },
@@ -84,25 +85,22 @@ const Calendarcard = () => {
     }
   };
 
-  // const renderDayInPicker = (
-  //   date,
-  //   dayComponent
-  // ) => {
-  //   if (daysWithDot.includes(date.format("YYYY/MM/DD"))) {
-  //     return (
-  //       <div className={classes.dayWithDotContainer}>
-  //         {dayComponent}
-  //         <div className={classes.dayWithDot} />
-  //       </div>
-  //     );
-  //   }
+  const renderDayInPicker = (
+    date,
+    dayComponent
+  ) => {
+    if (daysWithDot.includes(date.format("YYYY/MM/DD"))) {
+      return (
+        <div className={classes.dayWithDotContainer}>
+          {dayComponent}
+          <div className={classes.dayWithDot} />
+        </div>
+      );
+    }
 
-  //   return dayComponent;
-  // };
-
-  const handleClick = () => {
-    navigate("/CalendarLogs", { state: { date: date } });
+    return dayComponent;
   };
+
   return (
     <div>
       <Bg />
@@ -113,23 +111,22 @@ const Calendarcard = () => {
         onChange={setSelectedDay}
         shouldHighlightWeekends
       /> */}
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={6}>
-            <CalendarPicker
-              // date={date}
-              minDate={minDate}
-              maxDate={maxDate}
-              // onMonthChange={onPickerViewChange}
-              onChange={(newDate) => setDate(newDate)}
-              onClick={handleClick()}
-              views={["day"]}
-            />
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <Grid container justify="center" style={{ marginTop: 130 }}>
+            <Grid item xs={12} md={6}>
+              <CalendarPicker
+                // date={date}
+                minDate={minDate}
+                maxDate={maxDate}
+                // onMonthChange={onPickerViewChange}
+                onChange={(newDate) => setDate(newDate)}
+                // onClick={handleClick()}
+                views={["day"]}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-      </LocalizationProvider>
+        </LocalizationProvider>
       <BottomNavigationBar />
     </div>
   );
 };
-export default Calendarcard;
