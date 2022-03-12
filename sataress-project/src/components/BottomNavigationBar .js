8e7b63cb144801df11e11971293a0ae447d781a1
boss9755 <nongl3oss9755@gmail.com>
2022-03-12 15:23:31 +0700

    update

--- sataress-project/src/components/BottomNavigationBar .js
+++ sataress-project/src/components/BottomNavigationBar .js
@@ -21,23 +21,23 @@ const theme = createTheme({
   },
 });
 
-const useStyles = makeStyles({
-  root: {
-    background: "#FFE9E9",
-    boxShadow: "inset 0px 4px 4px rgba(251, 24, 24, 0.36)",
-    borderRadius: 5,
-  },
-});
+// const useStyles = makeStyles({
+//   root: {
+//     background: "#FFE9E9",
+//     boxShadow: "inset 0px 4px 4px rgba(251, 24, 24, 0.36)",
+//     borderRadius: 5,
+//   },
+// });
 
 
 export default function BottomNavigationBar() {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <Paper className={classes.root} sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+        <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
           elevation={10}>
-          <BottomNavigation className={classes.root}>
+          <BottomNavigation>
             <Link to="/">
               <HomeIcon color="Black"
                 sx={{ fontSize: 35, mx: 2, mt: 1.5 }} />
