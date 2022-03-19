ada031f5d995141c570cca9c92937ad4262d2768
oakntk <oakoakku@hotmail.com>
2022-03-19 15:37:27 +0700

    oak

--- sataress-project/src/components/calendar/CalendarLogs.js
+++ sataress-project/src/components/calendar/CalendarLogs.js
@@ -9,8 +9,6 @@ import { AuthContext } from "../Auth";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
 
 const Bg = withTheme(styled.div`
   position: fixed;
