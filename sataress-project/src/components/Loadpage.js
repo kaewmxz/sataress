87da21e8d7427dddb5e12a4f4b630c6d20af79e2
oakntk <oakoakku@hotmail.com>
2022-04-12 14:33:02 +0700

    eiei

--- sataress-project/src/components/Loadpage.js
+++ sataress-project/src/components/Loadpage.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+import { Grid } from "@material-ui/core";
 
-export default function Loadpage() {
-  return <div>Loading...</div>;
+export default function CircularIndeterminate() {
+  return (
+    <Box sx={{ display: 'flex' , mt:50}}>
+       <Grid container justifyContent="center">
+      <CircularProgress />
+      </Grid>
+    </Box>
+  );
 }
--- sataress-project/src/components/popup/PopupGratitude.js
+++ sataress-project/src/components/popup/PopupGratitude.js
@@ -3,7 +3,6 @@ import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
